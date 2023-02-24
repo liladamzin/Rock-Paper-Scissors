3ab83e7e5bdd0a0770734cee79f09b7cecafec85
Adamzin <tell.adamzin@gmail.com>
2023-02-23 21:28:10 -0300

    Update rock_paper_scissors.py

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,8 +1,8 @@
-# IMPORTANDO BIBLIOTECA DE "INTELIGENCIA ARTIFICAL".
+# > IMPORTANDO BIBLIOTECA DE "INTELIGENCIA ARTIFICAL" ↓
 
 import random
 
-# PONTOS DO USUARIO E DO NPC
+# > PONTOS DO USUARIO E DO NPC ↓
 
 user_points = 0
 npc_points = 0
@@ -10,27 +10,18 @@ npc_points = 0
 options = ["r", "t", "p"]
 #           0    1    2
 
-# LOOPING ENQUANTO O JOGO ESTIVER FUNCIONANDO.
+# > LOOPING ENQUANTO O JOGO ESTIVER FUNCIONANDO ↓
 
 while True:
     user_choice = input("Digite:\nR(Pedra)\nT(Tesoura)\nP(Papel)\nou Q para sair.\n").lower()
-    # input Usado como interação ao user
-    # lower Vai detectar todos os caracteres maiusculos dentro do input e irá transforma-los em minusculos
-    # \n Quebra linha
 
-    if user_choice not in options:  # Repetição caso o usuário digitar uma letra que NÃO ESTEJA em options
+    if user_choice not in options:
         print("Escolha uma das letras disponíveis!")
         continue
 
-
-# FIM DO JOGO.
-
     if user_choice == 'q':
-        break  # Ao digitar "q" o break irá finalizar o game
+        break
 
-# ESCOLHA DO COMPUTADOR (NPC)
-
-    # randint Vai escolher um número de 0 a 2 de forma aleatória
     npc_choice = random.randint(0, 2)
     # 0:R / 1:T / 2:P
     npc_option = options[npc_choice]
@@ -38,7 +29,7 @@ while True:
     print("O NPC escolheu: " + npc_option)
 
 
-# VERIFICAÇÃO DO GANHADOR
+# > VERIFICAÇÃO DO GANHADOR ↓
 
     if user_choice == npc_option:
         print("Empate!")
@@ -58,12 +49,12 @@ while True:
         print("------------")
         user_points = user_points + 1
 
-    else:  # Se o user perder
+    else:
         print("Você Perdeu!")
         print("------------")
         npc_points = npc_points + 1
 
-# PONTUAÇÃO FINAL
+# > PONTUAÇÃO FINAL ↓
 
 print("Sua pontuação: " + str(user_points))
 print("NPC pontuação: " + str(npc_points))
